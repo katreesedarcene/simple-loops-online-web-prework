1493bb88c1fa71f148138cb6605aaa3b7a7afa1c
Kat B <katreesebarnes@gmail.com>
2019-02-28 12:02:12 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   loop do 
     phrase += 1 
     puts "Welcome to Flatiron School's Web Development Course!"
-    if phrase >= 7 
+    if phrase == number_of_times
       break
     end
 
